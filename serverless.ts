272748274e6b3a3eabb11e974e5071c267bad3a3
안유성 <nug22kr@gmail.com>
2023-06-05 00:17:48 +0900

    서버리스 이미지 최적화 1차

---
+++ serverless.ts
@@ -0,0 +1,54 @@
+import type { AWS } from "@serverless/typescript";
+import resources from "./s3-cloudfront";
+
+const config: AWS = {
+    service: "photo-optimizer-api",
+    frameworkVersion: "3",
+    provider: {
+        name: "aws",
+        runtime: "nodejs14.x",
+        region: "ap-northeast-2",
+        environment: {
+            BUCKET_NAME: process.env.BUCKET_NAME!,
+            ROOT_DOMAIN: process.env.ROOT_DOMAIN!,
+            SUB_DOMAIN: process.env.SUB_DOMAIN!,
+            INFRA_DOMAIN: process.env.INFRA_DOMAIN!,
+        },
+        iam: {
+            role: {
+                statements: [
+                    {
+                        Action: ["s3:PutObject", "s3:GetObject"],
+                        Effect: "Allow",
+                        Resource: `arn:aws:s3:::${process.env.BUCKET_NAME}/*`,
+                    },
+                ],
+            },
+        },
+    },
+    custom: {
+        scripts: {
+            hooks: {
+                "webpack:package:packageModules":
+                    "cp jpegoptim.tar.gz .webpack/service",
+            },
+        },
+    },
+    functions: {
+        optimizeAndUpload: {
+            handler: "handler.optimizeAndUpload",
+            events: [
+                {
+                    httpApi: {
+                        path: "/optimize-and-upload",
+                        method: "put",
+                    },
+                },
+            ],
+        },
+    },
+    plugins: ["serverless-plugin-scripts", "serverless-webpack"],
+    resources,
+};
+
+export = config;
